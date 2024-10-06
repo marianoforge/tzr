d8b61c33c855d3e5d929ecd453892e4a1bb8b4da
Mariano De Simone <desimone.mariano@gmail.com>
2024-10-07 01:55:49 +0200

    CRF Security

--- lib/csrf.ts
+++ lib/csrf.ts
@@ -2,28 +2,30 @@ import { nanoid } from "nanoid";
 import { NextApiRequest, NextApiResponse } from "next";
 import { serialize, parse } from "cookie";
 
-// Generate a CSRF token and set it in a cookie
-export function setCsrfCookie(res: NextApiResponse) {
-  const token = nanoid(); // Generate a random token
-  const csrfCookie = serialize("csrfToken", token, {
-    httpOnly: true, // Prevent JavaScript access
-    secure: process.env.NODE_ENV === "production", // Use HTTPS in production
-    sameSite: "strict", // Prevent CSRF attacks
-    path: "/", // Make the cookie accessible across the site
+// Generar un token CSRF y establecerlo en una cookie
+export function setCsrfCookie(res: NextApiResponse): string {
+  const token: string = nanoid(); // Genera un token aleatorio
+  const csrfCookie: string = serialize("csrfToken", token, {
+    httpOnly: true, // Evitar acceso desde JavaScript
+    secure: process.env.NODE_ENV === "production", // Usar HTTPS en producción
+    sameSite: "strict", // Prevenir ataques CSRF
+    path: "/", // Hacer que la cookie sea accesible en todo el sitio
   });
-  res.setHeader("Set-Cookie", csrfCookie); // Set the CSRF token in the response cookies
+  res.setHeader("Set-Cookie", csrfCookie); // Establecer la cookie con el token CSRF en la respuesta
   return token;
 }
 
-// Validate the CSRF token
-export function validateCsrfToken(req: NextApiRequest) {
-  const cookies = parse(req.headers.cookie || ""); // Parse cookies from the request
-  const csrfTokenFromCookie = cookies["csrfToken"];
-  const csrfTokenFromHeader = req.headers["csrf-token"];
+// Validar el token CSRF
+export function validateCsrfToken(req: NextApiRequest): boolean {
+  const cookies: { [key: string]: string } = parse(req.headers.cookie || ""); // Parsear las cookies de la solicitud
+  const csrfTokenFromCookie: string | undefined = cookies["csrfToken"];
+  const csrfTokenFromHeader: string | undefined = req.headers["csrf-token"] as
+    | string
+    | undefined;
 
   if (!csrfTokenFromCookie || !csrfTokenFromHeader) {
     return false;
   }
 
-  return csrfTokenFromCookie === csrfTokenFromHeader; // Compare cookie and header tokens
+  return csrfTokenFromCookie === csrfTokenFromHeader; // Comparar el token de la cookie con el token del encabezado
 }
