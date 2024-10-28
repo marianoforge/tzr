50506175fb5979a6ab367aee8614157c4e2e6185
Mariano De Simone <desimone.mariano@gmail.com>
2024-10-28 08:46:22 +0100

    Agentes filtro cerrada

--- utils/calculationsAgents.ts
+++ utils/calculationsAgents.ts
@@ -2,25 +2,29 @@ import { Operation } from '@/types';
 
 // Function to calculate adjusted broker fees
 export const calculateAdjustedBrokerFees = (operations: Operation[]) =>
-  operations.reduce((acc: number, op: Operation) => {
-    const isHalfOperation =
-      op.user_uid &&
-      op.user_uid_adicional &&
-      op.user_uid !== op.user_uid_adicional;
+  operations
+    .filter((op) => op.estado === 'Cerrada') // Filtrar operaciones cerradas
+    .reduce((acc: number, op: Operation) => {
+      const isHalfOperation =
+        op.user_uid &&
+        op.user_uid_adicional &&
+        op.user_uid !== op.user_uid_adicional;
 
-    return acc + op.honorarios_broker * (isHalfOperation ? 0.5 : 1);
-  }, 0);
+      return acc + op.honorarios_broker * (isHalfOperation ? 0.5 : 1);
+    }, 0);
 
 // Function to calculate total operations
 export const calculateTotalOperations = (operations: Operation[]) =>
-  operations.reduce((total, op) => {
-    const isHalfOperation =
-      op.user_uid &&
-      op.user_uid_adicional &&
-      op.user_uid !== op.user_uid_adicional;
-    const isSingleOperation = op.user_uid && !op.user_uid_adicional;
-    return total + (isHalfOperation ? 0.5 : 0) + (isSingleOperation ? 1 : 0);
-  }, 0);
+  operations
+    .filter((op) => op.estado === 'Cerrada') // Filtrar operaciones cerradas
+    .reduce((total, op) => {
+      const isHalfOperation =
+        op.user_uid &&
+        op.user_uid_adicional &&
+        op.user_uid !== op.user_uid_adicional;
+      const isSingleOperation = op.user_uid && !op.user_uid_adicional;
+      return total + (isHalfOperation ? 0.5 : 0) + (isSingleOperation ? 1 : 0);
+    }, 0);
 
 // Function to calculate total buyer tips
 export const calculateTotalBuyerTips = (operations: Operation[]) =>
