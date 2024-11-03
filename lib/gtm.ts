b35cd3728e52130e5a1a929cdc90f4ded473408a
Mariano De Simone <desimone.mariano@gmail.com>
2024-11-03 14:24:59 +0100

    GTM

---
+++ lib/gtm.ts
@@ -0,0 +1,9 @@
+export const GTM_ID = 'GTM-K9WBH23B';
+
+export const GTM_SCRIPT = `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');
+`;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,31 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { GTM_ID, GTM_SCRIPT } from '../lib/gtm'; // Ajusta la ruta según la ubicación de tu archivo gtm.js
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-
+      <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: GTM_SCRIPT,
+          }}
+        />
+        {/* Fin Google Tag Manager */}
+      </Head>
       <SpeedInsights />
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* Fin Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
