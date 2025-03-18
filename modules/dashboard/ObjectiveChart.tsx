89a25ebb2942f3906b4884ec5c2718a9ec665268
Mariano De Simone <desimone.mariano@gmail.com>
2025-03-18 18:49:48 +0100

    Objetivos

--- modules/dashboard/ObjectiveChart.tsx
+++ modules/dashboard/ObjectiveChart.tsx
@@ -161,9 +161,7 @@ class ObjectiveChart extends PureComponent<ObjectiveChartProps> {
             </div>
             <h3 className="font-semibold text-mediumBlue text-base">
               {`Objetivo Anual de Ventas: $${formatNumber(
-                userData?.role === 'team_leader_broker'
-                  ? (totals.honorarios_broker_cerradas ?? 0)
-                  : (totals.honorarios_asesor_cerradas ?? 0)
+                totals.honorarios_broker_cerradas ?? 0
               )} / $${formatNumber(userData?.objetivoAnual ?? 0)}`}
             </h3>
           </>
